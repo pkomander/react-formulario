24aa419671253b7ee1a98d51f5ec4274561f396b
Paulo Ricardo Silva de Araújo <54222357+pkomander@users.noreply.github.com>
2020-09-08 19:02:52 -0300

    react-form
    Formulário de cadastro do curso da alura de react.js, utiliando react com a lib material ui para construção do layout.

---
+++ src/Components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import { TextField, Button, Switch, FormControlLabel } from "@material-ui/core";
+
+// quando trabalhamos com funções que retornão uma propriedade elas são stateless -> não guardam estado
+// utilizando o material ui, importando, e usando a tag Button
+// utilizando o material ui no TextField para campos do label e input
+// Switch aplica um switch de marcação para o formulario passando o tipo, variant e o color
+
+// so e possivel utilizar hooks no react dentro de funções
+// passando as propriedades por meio da descontrução
+function FormularioCadastro({ aoEnviar, validarCPF }) {
+    const [nome, setNome] = useState("");
+    const [sobrenome, setSobrenome] = useState("");
+    const [cpf, setCpf] = useState("");
+    const [promocoes, setPromocoes] = useState(true);
+    const [novidades, setNovidades] = useState(true);
+    const [erros, setErros] = useState({ cpf: { valido: true, texto: "" } });
+
+    return (
+        // validando formulario
+        <form onSubmit={(event) => {
+            event.preventDefault();
+            aoEnviar({ nome, sobrenome, cpf, novidades, promocoes });
+        }}
+        >
+            <TextField
+                value={nome}
+                onChange={event => {
+                    setNome(event.target.value);
+                }}
+                id="nome"
+                label="Nome"
+                variant="outlined"
+                margin="normal"
+                fullWidth
+            />
+            <TextField
+                value={sobrenome}
+                onChange={event => {
+                    setSobrenome(event.target.value);
+                }}
+                id="sobrenome"
+                label="Sobrenome"
+                variant="outlined"
+                margin="normal"
+                fullWidth
+            />
+            <TextField
+                value={cpf}
+                onChange={event => {
+                    setCpf(event.target.value);
+                }}
+
+                onBlur={(event) => {
+                    const ehValido = validarCPF(cpf);
+                    setErros({ cpf: ehValido });
+                }}
+                error={!erros.cpf.valido}
+                helperText={erros.cpf.texto}
+                id="cpf"
+                label="CPF"
+                variant="outlined"
+                margin="normal"
+                fullWidth
+            />
+
+            <FormControlLabel
+                label="Promoções"
+                control={<Switch
+                    checked={promocoes}
+                    onChange={(event) => {
+                        setPromocoes(event.target.checked);
+                    }} name="promoções" color="primary" />}
+            />
+            <FormControlLabel
+                label="Novidades"
+                control={<Switch
+                    checked={novidades}
+                    onChange={(event) => {
+                        setNovidades(event.target.checked);
+                    }} name="novidades" color="primary" />}
+            />
+
+            <Button type="submit" variant="contained" color="primary">
+                Cadastrar
+            </Button>
+        </form>
+    );
+}
+
+export default FormularioCadastro;
